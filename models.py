ae8cd51900ee9c9afefebd599b9a76e1486e60c3
Hitomi Carrasco <mithitomi68@gmail.com>
2026-06-20 12:08:57 -0500

    Add Categoria and updated Producto models

--- models.py
+++ models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+# 1. Creamos la categoría primero
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+# 2. Tu modelo de Producto actualizado
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    imagen = models.ImageField(upload_to='productos/', blank=True, null=True)
+    
+    # <-- Esta es la línea mágica que conecta el producto con su categoría
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.nombre
